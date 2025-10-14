64e1260c2fb0f7f288013c6a2fffa01ba6191b78
sidhantrajput1 <sidhant808310@gmail.com>
2025-10-14 08:35:59 +0530

    Ai Saas Quick Project

---
+++ Client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            This is DashBoard
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import AiTool from '../components/AiTool';
+import Testimonial from '../components/Testimonial';
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <AiTool />
+            <Testimonial />
+        </>
+    );
+}
+
+export default Home;
