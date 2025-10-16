21a98c982174e322ac339802c54ae7d9464dba03
sidhantrajput1 <sidhant808310@gmail.com>
2025-10-16 08:58:17 +0530

    Frontend ui part complete

--- Client/src/pages/Community.jsx
+++ Client/src/pages/Community.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { dummyPublishedCreationData } from "../assets/assets";
+import { Heart } from "lucide-react";
 
 const Community = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [creations, setCreations] = useState([]);
+  const { user } = useUser();
+
+  const fetchCreations = async () => {
+    setCreations(dummyPublishedCreationData);
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchCreations();
+    }
+  }, [user]);
+
+  return (
+    <div className="flex-1 h-full flex flex-col gap-4 p-6">
+      <h2 className="text-xl font-semibold text-slate-700">Creations</h2>
+
+      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
+        {creations.map((creation, index) => (
+          <div
+            key={index}
+            className="relative group inline-block pl-3 pt-3 w-full sm:w-1/2 lg:w-1/3"
+          >
+            <img
+              src={creation.content}
+              alt="AI Creation"
+              className="w-full h-full object-cover rounded-lg"
+            />
+
+            {/* Hover overlay */}
+            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg">
+              <p className="text-sm text-white px-3 text-center">
+                {creation.prompt}
+              </p>
+
+              <div className="flex gap-1 items-center">
+                <p>{creation.likes?.length || 0}</p>
+                <Heart
+                  className="min-w-5 h-5 cursor-pointer"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Community;
--- Client/src/pages/ReviewResume.jsx
+++ Client/src/pages/ReviewResume.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import { FileText, FileTextIcon, Scissors, Sparkles } from "lucide-react";
+
 
 const ReviewResume = () => {
-    return (
-        <div>
-            
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left column */}
+      <div className="min-w-[300px]">
+        <form
+          onSubmit={onSubmitHandler}
+          className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+        >
+          <div className="flex items-center gap-2 mb-4">
+            <Sparkles className="w-6 h-6 text-[#00DA83]" />
+            <h2 className="text-xl font-semibold">Resume Review</h2>
+          </div>
+
+          <p className="mt-6 text-sm font-medium">Upload Resume</p>
+          <input
+            type="file"
+            onChange={(e) => setInput(e.target.files[0])}
+            className="w-full p-2 mt-2 px-3 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+            required
+            accept="application/pdf"
+          />
+
+          <p className="mt-1 text-xs text-gray-500 font-medium">
+            Supports pdf resume only
+          </p>
+
+
+          <button
+            type="submit"
+            className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 transition"
+          >
+            <FileTextIcon className="w-5 h-5" />
+            Review Resume
+          </button>
+        </form>
+      </div>
+
+      {/* Right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-[24rem]">
+        <div className="flex items-center gap-3 mb-4">
+          <FileText className="w-5 h-5 text-[#00DA83]" />
+          <h2 className="text-xl font-semibold">Analysis Result</h2>
         </div>
-    );
-}
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <FileText className="w-9 h-9" />
+            <p>Upload an Resume and click "Review Resume" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ReviewResume;
