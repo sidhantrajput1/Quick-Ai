dc6489cccc2ac16234f18ea87663bc43f246d8e9
sidhantrajput1 <sidhant808310@gmail.com>
2025-10-17 08:01:09 +0530

    removeImageBackground

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -163,7 +163,7 @@ export const generateImage = async (req, res) => {
 export const removeImageBackground = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { prompt, publish } = req.body;
+    const { image } = req.file;
 
     const plan = req.plan;
     const free_usage = req.free_usage;
@@ -175,35 +175,27 @@ export const removeImageBackground = async (req, res) => {
       });
     }
 
-    const formData = new FormData();
-    formData.append("prompt", prompt);
-
-    const { data } = await axios.post(
-      "https://clipdrop-api.co/text-to-image/v1",
-      formData,
-      {
-        headers: {
-          "x-api-key": process.env.CLIPDROP_API_KEY,
-        },
-        responseType: "arraybuffer",
-      }
-    );
-
-    const base64Image = `data:image/png;base64,${Buffer.from(data, "binary").toString("base64")}`;
-    const { secure_url } = await cloudinary.uploader.upload(base64Image);
+    const {secure_url} = await cloudinary.uploader.upload(image.path, {
+      transformation : [
+        {
+          effect : 'background_removal',
+          background_removal  : 'remove_the_background '
+        }
+      ]
+    })
 
     await sql`
       INSERT INTO creations (user_id, prompt, content, type, publish)
-      VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})
+      VALUES (${userId}, "Remove background from image", ${secure_url}, 'image')
     `;
 
-    if (plan !== "premium") {
-      await clerkClient.users.updateUserMetadata(userId, {
-        privateMetadata: {
-          free_usage: free_usage + 1,
-        },
-      });
-    }
+    // if (plan !== "premium") {
+    //   await clerkClient.users.updateUserMetadata(userId, {
+    //     privateMetadata: {
+    //       free_usage: free_usage + 1,
+    //     },
+    //   });
+    // }
 
     res.json({ success: true, content: secure_url });
   } catch (error) {
